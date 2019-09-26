b4b1a91a61da165493a4c04cfd256d1eb7d44271
Charles Wines <ccrecorder@gmail.com>
2019-09-26 00:26:16 -0400

    classes done, 4 consts

--- script.js
+++ script.js
@@ -1,1 +1,122 @@
-console.log('Add validation!')
+class Validation {
+	constructor(test, errorMsg) {
+		this.test = test;
+		this.errorMsg = errorMsg;
+	}
+
+	validate(value) {
+		return this.test(value);
+	}
+}
+
+class Field {
+	constructor(inputDiv, validations) {
+		this.inputDiv = inputDiv;
+		this.validations = validations || [];
+	}
+
+	clearErrorMsgs() {
+		const fieldContainer = field.parentNode;
+		for (let msg of this.inputDiv.querySelector('.error-message')) {
+			msg.remove();
+		}
+	}
+
+	markValid() {
+		clearErrorMsgs(field);
+		this.inputDiv.classList.remove('input-invalid');
+		this.inputDiv.classList.add('input-valid');
+	}
+
+	markInvalid() {
+		clearErrorMsgs(field);
+		this.inputDiv.classList.remove('input-valid');
+		this.inputDiv.classList.add('input-invalid');
+	}
+
+	addErrorMsgs(errorMsgs) {
+		let fieldName = this.inputDiv.querySelector('label').innerText;
+		for (let msg of errorMsgs) {
+			const msgNode = document.createElement('p');
+			msgNode.classList.add('input-hint', 'text-danger', 'error-msg');
+			msgNode.innerText = `${fieldName} ${msg}`;
+			this.inputDiv.appendChilc(msgNode);
+		}
+	}
+
+	//what to put in querySelector?
+	getValue() {
+		const input = this.inputDiv.querySelector('');
+		const value = input.value;
+		return value;
+	}
+
+	validate() {
+		const value = this.getValue;
+		const errorMsgs = [];
+		for (let validation of this.validations) {
+			if (!validation.validate(value)) {
+				errorMsgs.push(validation.errorMsg);
+			}
+		}
+		if (errorMsgs.length === 0) {
+			this.markValid();
+		} else {
+			this.markInvalid();
+			this.addErrorMsgs(errorMsgs);
+		}
+		return errorMsgs.length === 0;
+	}
+}
+
+class Form {
+	constructor(domNode, fields) {
+		this.domNode = domNode;
+		this.fields = fields;
+	}
+	validate() {
+		let valid = true;
+		for (let field of this.fields) {
+			const fieldIsValid = field.validate();
+			if (!fieldIsValid) {
+				valid = false;
+			}
+		}
+		return valid;
+	}
+}
+
+function luhnCheck(val) {
+	var sum = 0;
+	for (var i = 0; i < val.length; i++) {
+		var intVal = parseInt(val.substr(i, 1));
+		if (i % 2 == 0) {
+			intVal *= 2;
+			if (intVal > 9) {
+				intVal = 1 + intVal % 10;
+			}
+		}
+		sum += intVal;
+	}
+	return sum % 10 == 0;
+}
+
+const presenceValidation = new Validation((value) => !!value, 'must not be blank');
+const nowOrFutureValidation = new Validation(function(dateStrToTest) {
+	if (!dateStrToTest) {
+		return true;
+	}
+
+	let dateToTest = new Date(dateStrToTest);
+	let now = new Date();
+	now.setUTCHours(0, 0, 0, 0);
+	dateToTest.setUTCHours(0, 0, 0, 0);
+
+	return dateToTest >= now;
+}, 'must be today or in the future');
+const validateCardNumber = new Validation(function(number) {
+	var regex = new RegExp('^[0-9]{16}$');
+	if (!regex.test(number)) return false;
+
+	return luhnCheck(number);
+}, 'must be a valid');
